dab8b010524927c598fa728aaa6d22fae07d512e
aikhe <ikeandrie.ro@gmail.com>
2025-08-16 18:58:38 +0800

    fix(types): specify vue.d.ts file to remove lsp 'cannot find' warning for vue imports

---
+++ src/views/LumiView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="lumi">
+    <h1>This is a page dedicated for my wife&lt;3</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
