1e9b3679a38b3496d24a6e912161a306fe79a4fb
aikhe <ikeandrie.ro@gmail.com>
2025-11-06 04:25:03 +0800

    feat(navbar): i'm really rusty + getting back to web dev yipi

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,31 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">HOME</RouterLink>
-      <RouterLink to="/test">TEST</RouterLink>
-      <RouterLink to="/lumi">LUMI</RouterLink>
-    </nav>
+    <div class="navbar container">
+      <div class="acedia">
+        <div class="acedia__box" />
+        <span class="sub-light">ACEDIA</span>
+      </div>
+
+      <nav class="sub-light">
+        <RouterLink to="/">HOME</RouterLink>
+        <RouterLink to="/test">TEST</RouterLink>
+        <RouterLink to="/lumi">LUMI</RouterLink>
+      </nav>
+
+      <button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="header__theme-toggle"
+        >
+          <path
+            d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z"
+          />
+        </svg>
+      </button>
+    </div>
   </header>
 
   <RouterView />
@@ -18,14 +38,20 @@ import { RouterLink, RouterView } from 'vue-router'
 header {
   display: flex;
   justify-content: center;
-  padding: 1rem;
+  align-items: center;
+  height: 4rem;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 nav {
   display: flex;
-  gap: 1rem;
-  font-size: 12pt;
-  font-family: ApercuMonoLight;
+  gap: 16rem;
 }
 
 nav a.router-link-exact-active {
@@ -40,5 +66,24 @@ nav a {
 
 nav a:hover {
   text-decoration: underline;
+}
+
+.acedia {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.6rem;
+}
+
+.acedia__box {
+  width: 0.7rem;
+  height: 0.7rem;
+  background-color: black;
+}
+
+.header__theme-toggle {
+  width: 1.2rem;
+  height: 1.2em;
+  fill: black;
 }
 </style>
