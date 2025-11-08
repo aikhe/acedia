252cd31d38cbf08da857bc67fe645c15991d07a9
aikhe <ikeandrie.ro@gmail.com>
2025-11-09 00:52:08 +0800

    feat(status): lots of relearning stuff i've forgot + the whiteboard + current status

---
+++ src/components/CurrentStatus.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="section--status">
+    <div class="container--status">
+      <div />
+      <div class="status">
+        <ul class="status__message">
+          <li>I’m Currently finding what this path leads to</li>
+        </ul>
+        <span class="status__label">Still Unknown</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.section--status {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1.2rem;
+}
+
+.container--status {
+  width: 1380px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.status {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  font-weight: 300;
+}
+
+.status__message {
+  color: #28ae3c;
+}
+
+ul li {
+  padding-left: 0.2rem;
+}
+
+.status__label {
+  opacity: 0.4;
+}
+</style>
---
+++ src/layouts/HeroLayout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useCounterStore } from '../stores/counter'
+import { storeToRefs } from 'pinia'
+
+const counter = useCounterStore()
+
+const { count } = storeToRefs(counter)
+</script>
+
+<template>
+  <section class="">
+    <div class="container">
+      <h1>acedia</h1>
+      <p>ike</p>
+      <p>d5d 6c756d69 6d616f6d69</p>
+      <p>{{ count }}</p>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
